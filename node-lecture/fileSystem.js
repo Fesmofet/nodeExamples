547b5b60337d5a85a880196b2523c0f9837c58af
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-05 14:48:50 +0300

    init2

---
+++ node-lecture/fileSystem.js
@@ -0,0 +1,105 @@
+const fs = require('fs')
+/**
+ *  принимает три параметра
+ *  путь к файлу;
+ *  кодировка;
+ *  callback-функция, вызываемая после получения содержимого файла.
+ */
+
+fs.readFile(`${__dirname}/wobj.json`, {encoding: 'utf8'}, (err,data) => {
+  console.log('yo')
+})
+// try {
+//   const content = fs.readFileSync(`${__dirname}/wobj.json`, 'utf8')
+//
+// } catch (error) {
+//   console.log(error)
+// }
+
+/**
+ * Методы readFile() и readFileSync() для работы с файлами используют Buffer.
+ *  Но есть и другой способ считать содержимое файла: создать поток с помощью Node.js
+ *  fs.createReadStream(). Любой поток в Node.js является экземпляром класса
+ *  EventEmitter, который позволяет обрабатывать возникающие в потоке события.
+ */
+
+const stream = fs.createReadStream(`${__dirname}/wobj.json`, 'utf8')
+stream.on('data', (data) => console.log('yo'))
+stream.on('error', (err) => console.log(`Err: ${err}`))
+
+/**
+ * Использование потока имеет ряд преимуществ перед Buffer:
+ * Меньшее использование памяти за счет чтения содержимого по частям;
+ * Для объемных файлов время между запросом и ответом существенно сокращается
+ * за счет того, что данные начинают поступать по частям, а не после полной загрузки;
+ * озможность перенаправить данные в другой поток с помощью метода pipe().
+ */
+fs.readdir(__dirname, 'utf8', (err, files) => {
+  if (err) throw err
+
+  console.log(files)
+})
+
+try {
+  const files = fs.readdirSync(__dirname, 'utf8')
+  console.log(files)
+} catch (e) {
+  console.log(e)
+}
+/**
+ * В Node.js файлы могут быть записаны также синхронно и асинхронно.
+ * Для асинхронной записи имеется метод writeFile(), принимающий следующие аргументы:
+ */
+fs.writeFile(
+  `${__dirname}/data.txt`,
+  'File Content',
+  'utf8',
+  (err) => {
+    if (err) throw err
+
+    console.log('Done')
+  }
+)
+
+try {
+  fs.writeFileSync(`${__dirname}/data.txt`, 'File Content', 'utf8')
+  console.log('Done')
+} catch (e) {
+  console.log(e)
+}
+
+/**
+ * Методы writeFile() и writeFileSync() перезаписывают уже имеющуюся в файле
+ * информацию новыми данными. Если вам нужно внести новые данные без удаления
+ * старых, используйте методы appendFIle() и appendFileAsync(), которые имеют
+ * идентичные параметры.
+ */
+fs.appendFile(
+  `${__dirname}/data.txt`,
+  '\nFile Content 2',
+  'utf8',
+  (err) => {
+    if (err) throw err
+
+    console.log('Done')
+  }
+)
+
+/**
+ * Для записи файла через потока ввода имеется метод fs.createWriteStream(),
+ * который возвращает поток ввода и принимает два параметра:
+ * путь к файлу;
+ * объект со следующими настройками:
+ */
+
+const writeStream = fs.createWriteStream(
+  `${__dirname}/data.txt`,
+  'utf8'
+)
+
+writeStream.on('error', (err) => console.log(`Err: ${err}`))
+writeStream.on('finish', () => console.log('Done'))
+
+writeStream.write('First line\n')
+writeStream.write('Second line\n')
+writeStream.end()
---
+++ node-lecture/path.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+// Normalization
+console.log('normalization : ' + path.normalize('/test/test1//2slashes/1slash/tab/..'));
+
+// Join
+console.log('join path : ' + path.join('/test', 'test1', '2slashes/1slash', 'tab', '..'));
+
+// Resolve
+console.log('resolve : ' + path.resolve('main.js'));
+
+// extName
+console.log('ext name : ' + path.extname('main.js'));
+
+// Basename
+console.log('basename : ' + path.basename('/srv/app/app.js')) //app.js
+console.log('basename : ' + path.basename('/srv/app/app.js', '.js')) //app
+
+// Dirname
+console.log('dirname : ' + path.dirname('/srv/app/app.js') ) // \srv\app
+
+// Absolute
+console.log('absolute : ' + path.isAbsolute('/srv/app/app.js')) //true
+console.log('absolute : ' + path.isAbsolute('srv/app/app.js')) //false
+
+// Relative
+console.log('relative : ' + path.relative('/srv/app/app.js', '/srv/config/default.conf'))  // ..\..\config\default.conf
+
+// Resolve
+console.log('resolve : ' + path.resolve('/srv/app', 'app.js')) // D:\srv\app\app.js
