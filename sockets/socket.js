bef9dbd7b93f16f9183d55fe8654e48fd0deb964
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-11 19:41:39 +0300

    add socketIo

--- sockets/socket.js
+++ sockets/socket.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const host = '127.0.0.1';
-const port = 7090;
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+const host = '127.0.0.1';
+const port = 7090;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -13,9 +14,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
   socket.on('chat message', (msg) => {
     io.emit('chat message', `${socket.nickName}: ${msg}`);
   });
