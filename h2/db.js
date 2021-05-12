213ee6dbc19a0509ea96db5295f185bc1c99caf9
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-12 17:18:24 +0300

    connect h2

--- h2/db.js
+++ h2/db.js
@@ -11,9 +11,14 @@ const run = async () => {
   try {
     await client.connect();
     console.log('connected');
-
+    const res = await client.query('SELECT * FROM Users');
+    // const res = await client.query('CREATE TABLE USERS(id serial PRIMARY KEY, name character varying(20) NOT NULL )');
+    // const res = await client.query('INSERT INTO USERS (name) VALUES ($1);', ['Boris']);
+    console.log(res);
   } catch (err) {
     console.log(err);
+  } finally {
+    await client.end();
   }
 };
 run();
--- h2/readMe.md
+++ h2/readMe.md
@@ -2,3 +2,6 @@ $ docker pull croraf/h2-postgresql-server:1.0
 $ docker create -p 1521:1521 -p 81:81 -v /path/to/local/data_dir:/opt/h2-data --name=H2dbWithPostgres croraf/h2-postgresql-server:1.0
 $ docker start H2dbWithPostgres
 $ touch /path/to/local/data_dir/mydatabase.mv.db
+
+for connection http://localhost:81
+url - jdbc:h2:/opt/h2-data/mydatabase
