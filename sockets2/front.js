ad3271f3cf89c44bf4c10c8cb527f9386e497098
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-12 09:31:14 +0300

    add socket ws

--- sockets2/front.js
+++ sockets2/front.js
@@ -6,11 +6,12 @@ const formNickname = document.getElementById('formNickname');
 const inputNick = document.getElementById('inputNick');
 const messageButton = document.getElementById('messageButton');
 
-let socket = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket("ws://localhost:8080");
 
 formNickname.addEventListener('submit', function (e) {
   e.preventDefault();
   if (inputNick.value) {
+    socket.send(JSON.stringify({method: 'setNickname', value: inputNick.value}))
     inputNick.value = '';
     messageButton.removeAttribute("disabled")
     input.removeAttribute("disabled")
@@ -21,13 +22,25 @@ formNickname.addEventListener('submit', function (e) {
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   if (input.value) {
+    socket.send(JSON.stringify({method: 'sendAll', value: input.value}))
     input.value = '';
   }
 });
 
-socket.on('chat message', function(msg) {
-  var item = document.createElement('li');
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-});
+socket.onmessage = function(message) {
+  let data;
+  try {
+    data = JSON.parse(message.data)
+  } catch (error) {
+    return console.log(error)
+  }
+
+  switch (data.type) {
+    case 'generalChat':
+      var item = document.createElement('li');
+      item.textContent = data.value;
+      messages.appendChild(item);
+      window.scrollTo(0, document.body.scrollHeight);
+      break;
+  }
+};
--- sockets2/server.js
+++ sockets2/server.js
@@ -5,6 +5,27 @@ const app = express();
 const host = '127.0.0.1';
 const port = 8080;
 
-const server = app.listen(port, () => console.log(`Listening on ${port}`));
+const server = app.listen(port, () => console.log(`Server listens http://${host}:${port}`));
 
 const wss = new SocketServer({ server, path: '/' });
+
+wss.on('connection', (ws) => {
+  console.log('Got connection from new peer');
+  ws.on('message', (message) => {
+    try {
+      message = JSON.parse(message)
+    } catch (error) {
+      return console.log(error)
+    }
+    switch (message.method) {
+      case 'setNickname':
+        ws.nickName = message.value
+        break;
+      case 'sendAll':
+        wss.clients.forEach((client) => {
+          client.send(JSON.stringify({type: 'generalChat', value: `${ws.nickName}: ${message.value}`}))
+        })
+        break;
+    }
+  })
+})
